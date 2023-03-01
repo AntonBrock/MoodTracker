7a3b0dd43772b59adfa8d2f072f891881358310e
Anton Dobrynin <anton.brock1@gmail.com>
2023-03-01 12:38:56 +0700

    Добавлен новый таргет - MoodMap и схемы для авторизации

---
+++ MoodMap/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  MoodMap
+//
+//  Created by ANTON DOBRYNIN on 01.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var coordinator: BaseViewCoordinator
+    @StateObject var viewRouter = ViewRouter()
+    @State var isHiddenTabBar: Bool = false
+        
+    var body: some View {
+        ZStack {
+            TabBarView(viewRouter: viewRouter,
+                       coordinator: coordinator)//isHiddenTabBar: $isHiddenTabBar
+            if coordinator.showAuthLoginView {
+                VStack {}
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .background(.black.opacity(0.7))
+                    .transition(.opacity)
+                
+                AuthMethodsView(dismiss: {
+                    withAnimation {
+                        coordinator.showAuthLoginView.toggle()
+                    }
+                }, openAboutRegistration: {
+                    withAnimation {
+                        coordinator.isShowingWhyResgistration.toggle()
+                    }
+                })
+                .zIndex(999999)
+            }
+            
+            if coordinator.isShowingWhyResgistration {
+                WhyRegistrationInfoView {
+                    withAnimation {
+                        coordinator.isShowingWhyResgistration.toggle()
+                    }
+                    withAnimation {
+                        coordinator.showAuthLoginView.toggle()
+                    }
+                } dismiss: {
+                    coordinator.isShowingWhyResgistration.toggle()
+                }
+                .transition(.move(edge: .bottom))
+            }
+        }
+    }
+}
+
