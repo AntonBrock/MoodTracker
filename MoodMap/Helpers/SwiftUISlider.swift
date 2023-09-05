6c99568d4b554847cc994e2ec31f1c4ddff8a1ee
Anton Dobrynin <a.dobrynin@amarkets.com>
2023-09-05 15:59:52 +0200

    Почитисл код + исправлено отображение текста под эмоциями, теперь текст всегда помещается в 1 линию и уменьшается если слишком большой

--- MoodMap/Helpers/SwiftUISlider.swift
+++ MoodMap/Helpers/SwiftUISlider.swift
@@ -13,9 +13,6 @@ struct SwiftUISlider: UIViewRepresentable {
         
         var thumbView: UIView?
         
-//        var dragBegan: () -> Void = {}
-//        var dragEnded: () -> Void = {}
-        
         override init(frame: CGRect = .zero) {
             super.init(frame: frame)
             
@@ -45,22 +42,11 @@ struct SwiftUISlider: UIViewRepresentable {
         init(value: Binding<Double>) {
             self.value = value
         }
-        
-//        @objc func beganDraging(_ sender: UISlider) {
-//            print("2")
-//            self.value.wrappedValue = Double(sender.value)
-//        }
-        
+
         @objc func valueChanged(_ sender: UISlider) {
-            
             let roundedValue = Float(round(sender.value / 10) * 10).rounded(.toNearestOrEven)
             self.value.wrappedValue = Double(roundedValue)
-
         }
-//
-//        @objc func endingDraging(_ sender: UISlider) {
-//            print("3")
-//        }
     }
 
     var thumbSize: CGRect = CGRect(x: 0, y: 0, width: 25, height: 25)
@@ -69,9 +55,7 @@ struct SwiftUISlider: UIViewRepresentable {
     var thumbColor: UIColor = .white
     var minTrackColor: UIColor?
     var maxTrackColor: UIColor?
-    
-//    var step: CGFloat?
-    
+        
     var minValue: CGFloat = 0.0
     var maxValue: CGFloat = 50.0
     
@@ -83,31 +67,22 @@ struct SwiftUISlider: UIViewRepresentable {
     func makeUIView(context: Context) -> UXSlider {
         let slider = UXSlider(frame: .zero)
         
-//        slider.dragBegan = dragBegan
-//        slider.dragMoved = dragMoved
-//        slider.dragEnded = dragEnded
-        
         customizeSlider(slider)
         
-        slider.addTarget(context.coordinator,
-                         action: #selector(Coordinator.valueChanged(_:)),
-                         for: .valueChanged
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged(_:)),
+            for: .valueChanged
         )
-        
-//        slider.addTarget(context.coordinator,
-//                         action: #selector(Coordinator.beganDraging(_:)),
-//                         for: .editingDidBegin)
-        
         return slider
     }
     
     func customizeSlider(_ slider: UXSlider) {
         slider.minimumTrackTintColor = minTrackColor
         slider.maximumTrackTintColor = maxTrackColor
-        slider.value = Float(round(value).rounded(.toNearestOrEven))   // if stepped
+        slider.value = Float(round(value).rounded(.toNearestOrEven))
         slider.maximumValue = Float(maxValue)
         
-        // create slide thumb
         let thumbView = UIView()
         thumbView.alpha = 1
         thumbView.backgroundColor = thumbColor
@@ -117,7 +92,6 @@ struct SwiftUISlider: UIViewRepresentable {
         thumbView.layer.borderWidth = thumbBorderWidth
         thumbView.layer.borderColor = thumbBorderColor.cgColor
         
-        // render thumbView as image, then set it as the thumb view image
         let imageRenderer = UIGraphicsImageRenderer(bounds: thumbView.bounds)
         let thumbImage = imageRenderer.image { rendererContext in
             thumbView.layer.render(in: rendererContext.cgContext)
@@ -125,33 +99,16 @@ struct SwiftUISlider: UIViewRepresentable {
         
         slider.setThumbImage(thumbImage, for: .normal)
         slider.thumbView = thumbView
-        
-        // set drop shadow appearance
-//        slider.layer.shadowColor = UIColor.black.cgColor
-//        slider.layer.shadowOpacity = 0.2
-//        slider.layer.shadowOffset = CGSize(width: 0, height: 3)
-//        slider.layer.shadowRadius = 5
-//
         slider.isContinuous = true
     }
     
     func updateUIView(_ slider: UXSlider, context: Context) {
-        // Coordinating data between UIView and SwiftUI view
-                        
         customizeSlider(slider)
         
         slider.addTarget(context.coordinator,
                          action: #selector(Coordinator.valueChanged(_:)),
                          for: .valueChanged
         )
-        
-//        slider.addTarget(context.coordinator,
-//                         action: #selector(Coordinator.beganDraging(_:)),
-////                         for: .touchDown)
-////
-//        slider.addTarget(context.coordinator,
-//                         action: #selector(Coordinator.endingDraging(_:)),
-//                         for: .touchUpInside)
     }
     
     func makeCoordinator() -> SwiftUISlider.Coordinator {
